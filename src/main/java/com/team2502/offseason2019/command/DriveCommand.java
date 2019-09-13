6a3d6cafb72854a51bfb8bbaa247d06ebfd38a83
Ishan Shetty <ishan.h.shetty@gmail.com>
2019-09-13 13:58:11 -0500

    Finished Drivetrain Command

--- src/main/java/com/team2502/offseason2019/command/DriveCommand.java
+++ src/main/java/com/team2502/offseason2019/command/DriveCommand.java
@@ -1,4 +1,32 @@
 package com.team2502.offseason2019.command;
 
-public class DriveCommand {
+import com.team2502.offseason2019.Robot;
+import com.team2502.offseason2019.subsystem.DrivetrainSubsystem;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class DriveCommand extends Command {
+
+    public DriveCommand() {
+        this(DrivetrainSubsystem.TeleopMode.VELOCITY);
+    }
+
+    public DriveCommand(DrivetrainSubsystem.TeleopMode teleopMode) {
+        requires(Robot.DRIVETRAIN);
+        Robot.DRIVETRAIN.setTeleopMode(teleopMode);
+    }
+
+    @Override
+    protected void execute() {
+        Robot.DRIVETRAIN.runTeleop();
+    }
+
+    @Override
+    protected void end() {
+        Robot.DRIVETRAIN.stopWheels();
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
 }
